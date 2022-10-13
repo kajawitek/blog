ee963a4d04f0324053c401d735efc440c06c11d8
kajawitek <kajawasiolek@gmail.com>
2022-10-13 20:01:52 +0200

    feat: add first name and last name to author model and to registrations views

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,10 @@ class Author < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, presence: true
+
+  def full_name
+    [first_name, last_name].join(' ')
+  end
 end
---
+++ db/migrate/20221013175044_add_first_name_and_last_name_to_author.rb
@@ -0,0 +1,6 @@
+class AddFirstNameAndLastNameToAuthor < ActiveRecord::Migration[7.0]
+  def change
+    add_column :authors, :first_name, :string, null: false
+    add_column :authors, :last_name, :string, null: false
+  end
+end
