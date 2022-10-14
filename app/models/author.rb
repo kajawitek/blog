60318bfa068885c1d6132e51669d1922fc67c7cb
Kaja Witek <kaja.wasiolek@gmail.com>
2022-10-14 14:48:57 +0200

    feat: create post model

--- app/models/author.rb
+++ app/models/author.rb
@@ -6,6 +6,8 @@ class Author < ApplicationRecord
 
   validates :first_name, :last_name, presence: true
 
+  has_many :posts, dependent: :destroy
+
   def full_name
     [first_name, last_name].join(' ')
   end
