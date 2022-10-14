d26af2c9e501472a93f5283af0c6cf98472ee71e
Kaja Witek <kaja.wasiolek@gmail.com>
2022-10-14 12:41:56 +0200

    fix: add hotwire to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :authors
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root to: 'home#index'
+  devise_for :authors, controllers: { sessions: 'authors/sessions' }
+      root to: 'home#index'
 end
