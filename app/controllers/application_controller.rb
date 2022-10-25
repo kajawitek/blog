5db044cdcb2328e257cb4fdaaba149bc611a5bb3
Kaja Witek <kaja.wasiolek@gmail.com>
2022-10-25 19:26:07 +0200

    feat: add pundit gem, configure it, add post_policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,21 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  include Pundit::Authorization
+
+  def pundit_user
+    current_author
+  end
+
+  rescue_from Pundit::NotAuthorizedError, with: :author_not_authorized
+
+  private
+
+  def author_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_back(fallback_location: root_path)
+  end
+
   protected
 
   def configure_permitted_parameters
